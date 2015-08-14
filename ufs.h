4d9176443cfda4b26b565e5bb73db3a76492a722
wuzhouhui <wuzhouhui250@gmail.com>
2015-08-15 06:49:18 +0800

    重定义 UFS_NAME_LEN, 使之能自动将目录项大小调整为 32 字节.
    将目录项调整为 32 字节, 是为了更有效得利用磁盘空间, 避免浪费

--- ufs.h
+++ ufs.h
@@ -23,7 +23,7 @@
 #define UFS_BLK_SIZE	(1 << UFS_BLK_SIZE_SHIFT)
 #define UFS_MAGIC		0x7594
 /* max length of file name, null terminator excluded */
-#define UFS_NAME_LEN        23
+#define UFS_NAME_LEN        (32 - sizeof(unsigned int) - 1)
 /* max length of path, null terminator excluded */
 #define UFS_PATH_LEN	1024
 /* # inode per block */
